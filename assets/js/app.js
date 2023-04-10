29c15bc84010ed530853abc34412073a88878220
dani <danigain1234@gmail.com>
2023-04-10 10:24:16 +0600

    add loaclstorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,32 +4,37 @@ let form = document.getElementById('form');
 let text  = form.querySelector('#tweet');
 let putDataDiv = form.parentElement.nextElementSibling;
 let tBody = document.querySelector('#tableBody');
-
+let localStorageitam = getFromLocalStorage();
 //--------------------event listener -------------------------//
 eventLitener();
 
 function eventLitener()
 {
       form.addEventListener("submit",setData);
+      document.addEventListener('DOMContentLoaded', putListToLocalStorage);
 }
 
 //---------------------- function ----------------------------------//
+
+
 
 function setData(event)
 {
      if(text.value != "")
      {
-        createTableRow();
+        setLocalStorage(text.value);
+        createTableRow(text.value);
         text.value = "";
      }
 }
 
-function createTableRow()
+function createTableRow(val)
 {
+    
     let tr = document.createElement('tr');
     tr.innerHTML= 
     `
-       <td scope="row">${text.value}</td>
+       <td scope="row">${val}</td>
        <td><img src="assets/img/dete_icon.png" alt="no image" height="20px" onclick="removeElement(this)"></td>
      `;
 
@@ -40,6 +45,48 @@ function createTableRow()
 //---------------------remove item ------------------------//
 function removeElement(event)
 {
-     event.parentElement.parentElement.remove()
+     let data = event.parentElement.parentElement.firstElementChild.innerText;
+     removeFromLocal(data);
+      event.parentElement.parentElement.remove()
 
+}
+
+function removeFromLocal(data)
+{
+     localStorageitam.forEach((value,index)=>{
+
+          if(value == data)
+          {
+               localStorageitam.splice(index , 1);
+          }
+          localStorage.clear();
+          localStorage.setItem('itam',JSON.stringify(localStorageitam));
+     })
+}
+
+
+
+//----------------------localstorage -----------------------//
+function putListToLocalStorage()
+{
+     localStorageitam.forEach((val) => {
+          createTableRow(val);
+     });
+}
+
+function getFromLocalStorage()
+{
+     let   itam = localStorage.getItem('itam');
+     if(!itam)
+     {
+           return [];
+     }else 
+     {
+          return JSON.parse(itam);
+     }
+}
+function setLocalStorage(itam)
+{
+     localStorageitam.push(itam);
+     localStorage.setItem('itam',JSON.stringify(localStorageitam));
 }
